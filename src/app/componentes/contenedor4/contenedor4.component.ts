67ad484555dc88ca70da17e869595c6a1a0c4446
Polsze <polbarrios835237@gmail.com>
2022-05-30 13:29:12 -0300

    json añadido

--- src/app/componentes/contenedor4/contenedor4.component.ts
+++ src/app/componentes/contenedor4/contenedor4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-contenedor4',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contenedor4.component.css']
 })
 export class Contenedor4Component implements OnInit {
-
-  constructor() { }
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+     this.datosPortfolio.obtenerDatos().subscribe(data => {
+        console.log(data);
+        this.miPortfolio=data;
+      });
   }
 
 }
